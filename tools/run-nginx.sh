58bec5a7fbce6d218f44ccf3d5db08a018ff8472
Andreas Konrad <andreas.konrad@gmx.net>
2025-12-10 21:44:48 +0100

    Improve run-nginx

--- tools/run-nginx.sh
+++ tools/run-nginx.sh
@@ -3,5 +3,5 @@
 DIR=${1:-$(pwd)}
 cd "$DIR" || exit 1
 
-echo "Running Nginx on port 8080 with root directory: $(pwd)"
+echo "Running Nginx on http://localhost:8080/ with root directory: $(pwd)"
 docker run --name "nginx" -d --rm -p 8080:80 -v "$(pwd):/usr/share/nginx/html:ro" nginx
