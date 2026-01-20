86be6f0b1d1d04f12fbd37ff96f31cd59a9bf719
Andreas Konrad <Andreas.Konrad@msg.group>
2026-01-20 11:39:55 +0100

    Improve run-nginx.ps1

--- tools/run-nginx.ps1
+++ tools/run-nginx.ps1
@@ -4,7 +4,14 @@ param(
     [string]$Dir = (Get-Location)
 )
 
-Set-Location $Dir
+Push-Location $Dir
+try {
+    $PWD_LINUX = (Get-Location).Path -replace '\\', '/'
+    $PWD_LINUX = $PWD_LINUX -replace 'c:/', "/mnt/c/"
 
-Write-Host "Running Nginx for $(Get-Location): http://localhost:8080"
-docker run --name "nginx" -d --rm -p 8080:80 -v "$(Get-Location):/usr/share/nginx/html:ro" nginx
+    Write-Host "Running Nginx for $(Get-Location): http://localhost:8080"
+    wsl docker run --name "nginx" -d --rm -p 8080:80 -v "${PWD_LINUX}:/usr/share/nginx/html:ro" nginx
+}
+finally {
+    Pop-Location
+}
