0dfb622ee8d18125be93121d1bacf63fe0f33e63
Andreas Konrad (msg) <Andreas.Konrad@dv-ma.de>
2026-01-20 14:32:10 +0100

    ps1 watch_adoc_2_html

---
+++ presentation/adoc_2_html.ps1
@@ -0,0 +1,1 @@
+docker run -v "c:\dev\projects\personal\konnika\presentation:/documents" asciidoctor/docker-asciidoctor asciidoctor-revealjs -r asciidoctor-diagram -o diva-start/diva-start.html diva-start/diva-start.adoc
---
+++ presentation/watch_adoc_2_html.ps1
@@ -0,0 +1,39 @@
+param (
+    [string]$PROJECT
+)
+
+if ([string]::IsNullOrWhiteSpace($PROJECT)) {
+    Write-Host "Usage: .\watch_adoc_2_html.ps1 -PROJECT <project-name>"
+    Write-Host "Example: .\watch_adoc_2_html.ps1 -PROJECT my-presentation"
+    Write-Host "Input expected: <project-name>/<project-name>.adoc"
+    Write-Host "Output will be: <project-name>/<project-name>.html"
+    exit 1
+}
+
+# Replace backslashes with forward slashes
+$PROJECT = $PROJECT.Replace('\', '/')
+
+$inputFile = "c:\dev\projects\personal\konnika\presentation\$PROJECT\$PROJECT.adoc"
+$outputFile = "c:\dev\projects\personal\konnika\presentation\$PROJECT\$PROJECT.html"
+
+Write-Host "Input file: $inputFile"
+Write-Host "Output file: $outputFile"
+
+while ($true) {
+    $shouldRebuild = $false
+
+    if (-not (Test-Path $outputFile)) {
+        $shouldRebuild = $true
+    }
+    elseif ((Get-Item $inputFile).LastWriteTime -gt (Get-Item $outputFile).LastWriteTime) {
+        $shouldRebuild = $true
+    }
+
+    if ($shouldRebuild) {
+        Write-Host "Rebuilding presentation..." -NoNewline
+        docker run -v "c:\dev\projects\personal\konnika\presentation:/documents" asciidoctor/docker-asciidoctor asciidoctor-revealjs -r asciidoctor-diagram -o $PROJECT/$PROJECT.html $PROJECT/$PROJECT.adoc
+        Write-Host " done."
+    }
+
+    Start-Sleep -Seconds 1
+}
