536c8065a18952e43e3691144e8fa92add8c5c73
Andreas Konrad (msg) <andreas.konrad@dv-ma.de>
2026-04-10 15:39:22 +0200

    Update git setup

--- setup-git.sh
+++ setup-git.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-read -p "Enter your name for git: " GIT_NAME
-read -p "Enter your email for git: " GIT_EMAIL
+read -p "Enter your name: " GIT_NAME
+read -p "Enter your email: " GIT_EMAIL
 
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 
-git config --global alias.ls "status"
-git config --global alias.co "checkout"
-git config --global alias.pr "pull --rebase"
-git config --global alias.logl "log --oneline --graph -20"
+cat .gitconfig >> ~/.gitconfig
